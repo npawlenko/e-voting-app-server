6a779a01a0e588753a04d65e0fc4874130a95df5
blinkz8 <blinkz8@protonmail.com>
2023-04-11 21:49:30 +0200

    Initial commit

---
+++ src/main/java/com/github/npawlenko/evotingapp/EVotingAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.npawlenko.evotingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVotingAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EVotingAppServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/npawlenko/evotingapp/EVotingAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.npawlenko.evotingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVotingAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
