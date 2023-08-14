5e3cbc3d3d51ededc31e7d57a456bd85eae01792
npawlenko <blinkz8@protonmail.com>
2023-08-14 13:41:16 +0200

    Add models and create schema initialization

---
+++ src/main/java/com/github/npawlenko/evotingapp/model/Poll.java
@@ -0,0 +1,47 @@
+package com.github.npawlenko.evotingapp.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "polls")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Poll {
+
+    @Id
+    @SequenceGenerator(
+            name = "polls_id_generator",
+            sequenceName = "polls_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "polls_id_generator",
+            strategy = GenerationType.SEQUENCE
+    )
+    private Long id;
+    @NotBlank
+    private String question;
+    @Column(name = "created_at")
+    @NotBlank
+    private LocalDateTime createdAt;
+    @Column(name = "expiresAt")
+    @NotBlank
+    private LocalDateTime expiresAt;
+
+
+    @ManyToOne
+    @JoinColumn(name = "creator_id", referencedColumnName = "id")
+    private User creator;
+    @OneToMany(mappedBy = "poll")
+    private List<PollAnswer> pollAnswers;
+    @OneToMany(mappedBy = "poll")
+    private List<Vote> votes;
+}
---
+++ src/main/java/com/github/npawlenko/evotingapp/model/Vote.java
@@ -0,0 +1,40 @@
+package com.github.npawlenko.evotingapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "votes")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vote {
+
+    @Id
+    @SequenceGenerator(
+            name = "votes_id_generator",
+            sequenceName = "votes_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "votes_id_generator",
+            strategy = GenerationType.SEQUENCE
+    )
+    private Long id;
+    @Column(name = "casted_at")
+    private LocalDateTime castedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "answer_id", referencedColumnName = "id")
+    private PollAnswer answer;
+    @ManyToOne
+    @JoinColumn(name = "poll_id", referencedColumnName = "id")
+    private Poll poll;
+    @ManyToOne
+    @JoinColumn(name = "voter_id", referencedColumnName = "id")
+    private User voter;
+}
