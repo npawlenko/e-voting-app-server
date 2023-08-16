bfee7a371ca4342f9132e2bb1e22ff8e50c2f6ff
npawlenko <blinkz8@protonmail.com>
2023-08-16 12:47:34 +0200

    Rename to UserService

--- src/main/java/com/github/npawlenko/evotingapp/user/UserService.java
+++ src/main/java/com/github/npawlenko/evotingapp/user/UserService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class DbUserDetailsService implements UserDetailsService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
