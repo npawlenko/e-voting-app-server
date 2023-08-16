78a923fe6a749226fe21b8e609648457f911eff4
npawlenko <blinkz8@protonmail.com>
2023-08-16 11:46:02 +0200

    Add GrantedAuthority interface to Role

--- src/main/java/com/github/npawlenko/evotingapp/model/User.java
+++ src/main/java/com/github/npawlenko/evotingapp/model/User.java
@@ -58,9 +58,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(
-                new SimpleGrantedAuthority(role.getRole().name())
-        );
+        return List.of(role);
     }
 
     @Override
