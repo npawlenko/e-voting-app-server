4a6e64e04b326c78c8a2e97fdf63bf8216db2956
Norbert Pawlenko <blinkz8@protonmail.com>
2023-08-21 19:22:09 +0200

    Fix UserGroup ManyToMany annotation  =column names

--- src/main/java/com/github/npawlenko/evotingapp/model/UserGroup.java
+++ src/main/java/com/github/npawlenko/evotingapp/model/UserGroup.java
@@ -33,8 +33,8 @@ public class UserGroup {
     @ManyToMany
     @JoinTable(
             name = "user_group_association",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "group_id")
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
     )
     private List<User> users;
 }
