317ccaaec491f419b5f8d97e0208517effc0fe25
Norbert Pawlenko <blinkz8@protonmail.com>
2023-09-10 19:49:49 +0200

    Fix tests

--- src/test/java/com/github/npawlenko/evotingapp/poll/PollServiceTest.java
+++ src/test/java/com/github/npawlenko/evotingapp/poll/PollServiceTest.java
@@ -1,30 +1,12 @@
 package com.github.npawlenko.evotingapp.poll;
 
-import static com.github.npawlenko.evotingapp.exception.ApiRequestExceptionReason.USER_NOT_LOGGED_IN;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import com.github.npawlenko.evotingapp.exception.ApiRequestException;
-import com.github.npawlenko.evotingapp.exception.ApiRequestExceptionReason;
 import com.github.npawlenko.evotingapp.model.Poll;
 import com.github.npawlenko.evotingapp.model.User;
 import com.github.npawlenko.evotingapp.poll.dto.PollRequest;
 import com.github.npawlenko.evotingapp.poll.dto.PollResponse;
 import com.github.npawlenko.evotingapp.utils.AuthenticatedUserUtility;
 import com.github.npawlenko.evotingapp.utils.AuthorizationUtility;
-
-import java.util.ArrayList;
-import java.util.Optional;
-
-import org.junit.jupiter.api.Disabled;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -32,6 +14,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static com.github.npawlenko.evotingapp.exception.ApiRequestExceptionReason.USER_NOT_LOGGED_IN;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ContextConfiguration(classes = {PollService.class})
 @ExtendWith(SpringExtension.class)
@@ -130,15 +121,15 @@ class PollServiceTest {
     @Test
     void testCreatePoll_CreatesPoll() {
         when(authenticatedUserUtility.getLoggedUser()).thenReturn(new User());
-        PollResponse pollResponse = new PollResponse();
-        when(pollMapper.pollToPollResponse(Mockito.<Poll>any())).thenReturn(pollResponse);
-        when(pollMapper.pollRequestToPoll(Mockito.<PollRequest>any())).thenReturn(new Poll());
-        when(pollRepository.save(Mockito.<Poll>any())).thenReturn(new Poll());
-        assertSame(pollResponse, pollService.createPoll(new PollRequest()));
+        PollResponse pollResponse = new PollResponse(1L, null, null, null, true, null, null, null);
+        when(pollMapper.pollToPollResponse(Mockito.any())).thenReturn(pollResponse);
+        when(pollMapper.pollRequestToPoll(Mockito.any())).thenReturn(new Poll());
+        when(pollRepository.save(Mockito.any())).thenReturn(new Poll());
+        assertSame(pollResponse, pollService.createPoll(new PollRequest("Question", LocalDateTime.MAX, true)));
         verify(authenticatedUserUtility).getLoggedUser();
-        verify(pollMapper).pollRequestToPoll(Mockito.<PollRequest>any());
-        verify(pollMapper).pollToPollResponse(Mockito.<Poll>any());
-        verify(pollRepository).save(Mockito.<Poll>any());
+        verify(pollMapper).pollRequestToPoll(Mockito.any());
+        verify(pollMapper).pollToPollResponse(Mockito.any());
+        verify(pollRepository).save(Mockito.any());
     }
 
     /**
@@ -148,7 +139,7 @@ class PollServiceTest {
     void testCreatePoll_ThrowsApiRequestException_UserNotLoggedIn() {
         when(authenticatedUserUtility.getLoggedUser())
                 .thenThrow(new ApiRequestException(USER_NOT_LOGGED_IN, "Args"));
-        assertThrows(ApiRequestException.class, () -> pollService.createPoll(new PollRequest()));
+        assertThrows(ApiRequestException.class, () -> pollService.createPoll(new PollRequest("Question", LocalDateTime.MAX, true)));
         verify(authenticatedUserUtility).getLoggedUser();
     }
 
@@ -158,18 +149,18 @@ class PollServiceTest {
     @Test
     void testUpdatePoll_UpdatesPoll() {
         when(authenticatedUserUtility.getLoggedUser()).thenReturn(new User());
-        doNothing().when(authorizationUtility).requireAdminOrOwnerPermission(Mockito.<User>any(), Mockito.<User>any());
-        PollResponse pollResponse = new PollResponse();
-        when(pollMapper.pollToPollResponse(Mockito.<Poll>any())).thenReturn(pollResponse);
-        when(pollMapper.pollRequestToPoll(Mockito.<PollRequest>any())).thenReturn(new Poll());
-        when(pollRepository.save(Mockito.<Poll>any())).thenReturn(new Poll());
+        doNothing().when(authorizationUtility).requireAdminOrOwnerPermission(Mockito.any(), Mockito.any());
+        PollResponse pollResponse = new PollResponse(1L, null, null, null, true, null, null, null);
+        when(pollMapper.pollToPollResponse(Mockito.any())).thenReturn(pollResponse);
+        when(pollMapper.pollRequestToPoll(Mockito.any())).thenReturn(new Poll());
+        when(pollRepository.save(Mockito.any())).thenReturn(new Poll());
         when(pollRepository.findById(Mockito.<Long>any())).thenReturn(Optional.of(new Poll()));
-        assertSame(pollResponse, pollService.updatePoll(1L, new PollRequest()));
+        assertSame(pollResponse, pollService.updatePoll(1L, new PollRequest("Question", LocalDateTime.MAX, true)));
         verify(authenticatedUserUtility).getLoggedUser();
-        verify(authorizationUtility).requireAdminOrOwnerPermission(Mockito.<User>any(), Mockito.<User>any());
-        verify(pollMapper).pollRequestToPoll(Mockito.<PollRequest>any());
-        verify(pollMapper).pollToPollResponse(Mockito.<Poll>any());
-        verify(pollRepository).save(Mockito.<Poll>any());
+        verify(authorizationUtility).requireAdminOrOwnerPermission(Mockito.any(), Mockito.any());
+        verify(pollMapper).pollRequestToPoll(Mockito.any());
+        verify(pollMapper).pollToPollResponse(Mockito.any());
+        verify(pollRepository).save(Mockito.any());
         verify(pollRepository).findById(Mockito.<Long>any());
     }
 
@@ -180,7 +171,7 @@ class PollServiceTest {
     void testUpdatePoll_ThrowsApiRequestException_UserNotLoggedIn() {
         when(authenticatedUserUtility.getLoggedUser())
                 .thenThrow(new ApiRequestException(USER_NOT_LOGGED_IN));
-        assertThrows(ApiRequestException.class, () -> pollService.updatePoll(1L, new PollRequest()));
+        assertThrows(ApiRequestException.class, () -> pollService.updatePoll(1L, new PollRequest("Question", LocalDateTime.MAX, true)));
         verify(authenticatedUserUtility).getLoggedUser();
     }
 
@@ -191,12 +182,12 @@ class PollServiceTest {
     @Test
     void testDeletePoll_DeletesPoll() {
         when(authenticatedUserUtility.getLoggedUser()).thenReturn(new User());
-        doNothing().when(authorizationUtility).requireAdminOrOwnerPermission(Mockito.<User>any(), Mockito.<User>any());
+        doNothing().when(authorizationUtility).requireAdminOrOwnerPermission(Mockito.any(), Mockito.any());
         doNothing().when(pollRepository).deleteById(Mockito.<Long>any());
         when(pollRepository.findById(Mockito.<Long>any())).thenReturn(Optional.of(new Poll()));
         pollService.deletePoll(1L);
         verify(authenticatedUserUtility).getLoggedUser();
-        verify(authorizationUtility).requireAdminOrOwnerPermission(Mockito.<User>any(), Mockito.<User>any());
+        verify(authorizationUtility).requireAdminOrOwnerPermission(Mockito.any(), Mockito.any());
         verify(pollRepository).findById(Mockito.<Long>any());
         verify(pollRepository).deleteById(Mockito.<Long>any());
     }
